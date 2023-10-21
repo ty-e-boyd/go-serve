7f6d2352eacf17dcac29e9a10750c09d653ee16a
Tyler Boyd <ty@tyboyd.dev>
2023-10-20 21:45:54 -0500

    added a form with a submission that POSTs to /form

--- server.go
+++ server.go
@@ -23,9 +23,38 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		_, err2 := fmt.Fprintf(w, "Unable to parse form. %v", err)
+		if err2 != nil {
+			return
+		}
+		return
+	}
+
+	_, err := fmt.Fprintf(w, "POST was successful")
+	if err != nil {
+		return
+	}
+
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	_, err = fmt.Fprintf(w, "Name: %s", name)
+	if err != nil {
+		return
+	}
+
+	_, err = fmt.Fprintf(w, "Address: %s", address)
+	if err != nil {
+		return
+	}
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Println("starting server on port 8080")
